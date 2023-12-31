525753c169cb3ff2ea6266b24dd0f3d59d98ad9f
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-12-31 18:03:01 -0400

    adds GPL

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -104,3 +104,31 @@ void *Util_Malloc (size_t const sz)
 
 	return data;
 }
+
+/*
+
+BDX                                             December 31, 2023
+
+Copyright (C) 2023 Misael Díaz-Maldonado
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+author: @misael-diaz
+source: src/util/util.cpp
+
+References:
+[0] A Koenig and B Moo, Accelerated C++ Practical Programming by Example.
+[1] MP Allen and DJ Tildesley, Computer Simulation of Liquids.
+[2] S Kim and S Karrila, Microhydrodynamics.
+
+*/
