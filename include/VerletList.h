42c4dcb75abd794e60fa41b200ce9456486f8e56
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-04-22 15:08:35 -0400

    adds `VerletList`

---
+++ include/VerletList.h
@@ -0,0 +1,51 @@
+#ifndef GUARD_BDX_VERLET_LIST_H
+#define GUARD_BDX_VERLET_LIST_H
+
+struct List;
+struct Particle;
+
+struct VerletList
+{
+	double d = 0;
+	Vector *dr = NULL;
+	List *__list__ = NULL;
+	VerletList(List *list, Vector *displacement);
+	size_t cap() const;
+	size_t numel() const;
+	void add(Particle *particle);
+	void clear();
+	const Particle **begin() const;
+	const Particle **end() const;
+	void *operator new(size_t size);
+	void operator delete(void *p);
+};
+
+#endif
+
+/*
+
+BDX                                             December 31, 2023
+
+Copyright (C) 2023 Misael Díaz-Maldonado
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+author: @misael-diaz
+source: include/VerletList.h
+
+References:
+[0] A Koenig and B Moo, Accelerated C++ Practical Programming by Example.
+[1] MP Allen and DJ Tildesley, Computer Simulation of Liquids.
+[2] S Kim and S Karrila, Microhydrodynamics.
+
+*/
